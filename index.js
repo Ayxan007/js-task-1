198b68e12643a14f6b0b958b4116aba64e0169aa
AyxanR <ahuseynli630@gmail.com>
2024-08-24 04:27:49 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// const person = { 
+//     firstname: "Umid",
+//     lastname: "Bekirov",
+//     fullName() {
+//       return `${this.firstname} ${this.lastname}`
+//     }
+//   }
+
+// function fullName (){ 
+//     return this.firstName +" "+this.lastName;
+//     } 
+//     console.log(person.fullName());
+
+
+// let fullAdress = 'Sea brezze resort, Bakı 1002'
+// console.log(fullAdress)
+// alert(fullAdress)
+
+
+// let grade= +prompt("balinizi yazin")
+// function grades(){
+//     if(grade<=200){
+//         alert("musabiqe neticesinde siz hec bir ixtisasa qebul olmadiniz")
+//     }
+//     else if( grade<=400){
+//         alert('musabiqe neticesinde siz orta neticeyle qebul oldunuz')
+//     }
+//     else if(grade<=700){
+//         alert('musabiqe neticesinde siz yuksek neticeyle qebul oldunuz')
+//     }
+// }
+// grades()
